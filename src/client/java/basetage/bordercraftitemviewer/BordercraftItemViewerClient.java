410e49ddd82fa49439b56f61e5734ff2f40769c1
Marc-Antoine Girard-Gagnon <maggagnon@etu.uqac.ca>
2025-03-11 14:10:31 -0400

    init

---
+++ src/client/java/basetage/bordercraftitemviewer/BordercraftItemViewerClient.java
@@ -0,0 +1,10 @@
+package basetage.bordercraftitemviewer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BordercraftItemViewerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
