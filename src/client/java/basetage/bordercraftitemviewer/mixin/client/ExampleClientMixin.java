009d42c1a6accad3583f80fb19ce553467cfb80d
Marc-Antoine Girard-Gagnon <maggagnon@etu.uqac.ca>
2025-03-13 00:33:14 -0400

    actually made it work yipee

--- src/client/java/basetage/bordercraftitemviewer/mixin/client/ExampleClientMixin.java
+++ src/client/java/basetage/bordercraftitemviewer/mixin/client/ExampleClientMixin.java
@@ -1,15 +1,21 @@
 package basetage.bordercraftitemviewer.mixin.client;
 
 import net.minecraft.client.MinecraftClient;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class ExampleClientMixin {
+	@Shadow @Final private static Logger LOGGER;
+
 	@Inject(at = @At("HEAD"), method = "run")
 	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftClient.run()V
+		// This code is injected into the start of MinecraftClient.run()
+		LOGGER.info("Hello from client!");
 	}
 }
